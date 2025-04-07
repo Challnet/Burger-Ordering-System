5452d8f6e41b2c641f23871a9bca160cd7b38319
Challnet <challnet@mail.ru>
2025-04-07 22:16:05 +0500

    final commit

--- Project/db.php
+++ Project/db.php
@@ -6,7 +6,7 @@ $dbname = "quickgrilldb2";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
+C
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
